4a39e0a5498382f8347c0e671b39f0bda9d0a542
Yonatan Perry <yonatan.perry@gmail.com>
2025-06-27 23:53:10 -0400

    meow in the code

--- epic/_cicd_dev/__init__.py
+++ epic/_cicd_dev/__init__.py
@@ -1,4 +1,4 @@
 # mock library for internal cicd development purposes
 
 def function():
-    return 100
+    return 101
--- tests/test_general.py
+++ tests/test_general.py
@@ -6,4 +6,4 @@ from epic._cicd_dev import function
 
 
 def test_function():
-    assert function() == 100
+    assert function() == 101
